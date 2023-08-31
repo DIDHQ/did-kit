5a42bb362d2d7f2ab3bd3e22dc7154909ba40767
Aliez <renzholy@gmail.com>
2023-08-31 20:33:57 +0900

    Update coin-type.ts

--- src/coin-type.ts
+++ src/coin-type.ts
@@ -1,13 +1,21 @@
 export enum CoinType {
+  PassKey = -1,
   Ethereum = 60,
   Tron = 195,
 }
 
 export function guessCoinType(address: string): CoinType | null {
+  if (/^ck[bt]1[qpzry9x8gf2tvdw0s3jn54khce6mua7l]{42,}$/i.test(address)) {
+    return CoinType.PassKey;
+  }
   if (/^0x[a-zA-Z0-9]{40}$/.test(address)) {
     return CoinType.Ethereum;
   }
-  if (/^T[A-Za-z1-9]{33}$/.test(address)) {
+  if (
+    /^T[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{33}$/.test(
+      address
+    )
+  ) {
     return CoinType.Tron;
   }
   return null;
