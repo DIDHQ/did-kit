ff78e71df902136658e287715bd20ce6e40a8837
Aliez <renzholy@gmail.com>
2023-09-18 14:47:50 +0900

    isBase64

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { Hex } from 'viem'
+import { isHex } from 'viem'
 import { CoinType, guessCoinType } from './coin-type'
 import { verifyDogecoinMessage } from './verify-message/dogecoin'
 import { verifyEvmMessage } from './verify-message/evm'
@@ -8,24 +8,36 @@ import { verifyTronMessage } from './verify-message/tron'
 export async function verifyMessage(
   address: string,
   message: string,
-  signature: Hex,
+  signature: string,
 ): Promise<boolean> {
   const coinType = guessCoinType(address)
   switch (coinType) {
     case CoinType.Dogecoin: {
-      return verifyDogecoinMessage(address, message, signature)
+      return isBase64(signature)
+        ? verifyDogecoinMessage(address, message, signature)
+        : false
     }
     case CoinType.Ethereum: {
-      return verifyEvmMessage(address, message, signature)
+      return isHex(signature)
+        ? verifyEvmMessage(address, message, signature)
+        : false
     }
     case CoinType.Nervos: {
-      return verifyNervosMessage(address, message, signature)
+      return isHex(signature)
+        ? verifyNervosMessage(address, message, signature)
+        : false
     }
     case CoinType.Tron: {
-      return verifyTronMessage(address, message, signature)
+      return isHex(signature)
+        ? verifyTronMessage(address, message, signature)
+        : false
     }
     default: {
       throw new Error('unknown address')
     }
   }
+}
+
+function isBase64(value: string) {
+  return /^[a-zA-Z0-9+/]={0,3}$/.test(value)
 }
--- src/verify-message/dogecoin.ts
+++ src/verify-message/dogecoin.ts
@@ -1,10 +1,9 @@
-import { Hex } from 'viem'
 import { verify } from 'bitcoinjs-message'
 
 export async function verifyDogecoinMessage(
   address: string,
   message: string,
-  signature: Hex,
+  signature: string,
 ) {
   return verify(message, address, signature, '\x19Dogecoin Signed Message:\n')
 }
