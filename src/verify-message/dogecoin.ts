91728f946e66cbb48c1c3cf6596f1ff6a61c55d1
Aliez <renzholy@gmail.com>
2023-09-18 14:31:42 +0900

    verify

--- src/verify-message/dogecoin.ts
+++ src/verify-message/dogecoin.ts
@@ -1,7 +1,10 @@
 import { Hex } from "viem";
+import { verify } from "bitcoinjs-message";
 
 export async function verifyDogecoinMessage(
   address: string,
   message: string,
   signature: Hex
-) {}
+) {
+  return verify(message, address, signature, "\x19Dogecoin Signed Message:\n");
+}
