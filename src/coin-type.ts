4a8bb42a8018854d1cd56f2a924c941cdf2a2399
Aliez <renzholy@gmail.com>
2023-09-18 14:26:11 +0900

    Update coin-type.ts

--- src/coin-type.ts
+++ src/coin-type.ts
@@ -1,10 +1,14 @@
 export enum CoinType {
+  Dogecoin = 3,
   Ethereum = 60,
   Tron = 195,
   Nervos = 309,
 }
 
 export function guessCoinType(address: string): CoinType | null {
+  if (/^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$/.test(address)) {
+    return CoinType.Dogecoin;
+  }
   if (/^0x[a-fA-F0-9]{40}$/.test(address)) {
     return CoinType.Ethereum;
   }
